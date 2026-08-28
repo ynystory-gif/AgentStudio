30ad6d5b39a28894e1b1553a1b9be8eb601f3898
윤우진 <ynystory@gmail.com>
2026-08-28 23:28:57 +0900

    내용확인 필요

--- frontend/public/runtime-config.js
+++ frontend/public/runtime-config.js
@@ -1,6 +1,6 @@
 ﻿window.__AGENTSTUDIO_CONFIG__ = {
   BACKEND_HOST: "127.0.0.1",
-  BACKEND_PORT: 8002,
-  FRONTEND_PORT: 5174,
-  AGENTSTUDIO_ROOT: "C:\\AI\\AgentStudio"
+  BACKEND_PORT: 8001,
+  FRONTEND_PORT: 5173,
+  AGENTSTUDIO_ROOT: "F:\\Source\\repos\\Theanova\\AI\\AgentStudio"
 };
