e6902f9da0788013273bb7c58dec839f827d19d3
ynystory-gif <ynystory@gmail.com>
2026-08-29 02:47:01 +0900

    v5.423 mount dynamic layout source enhancer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthGate } from './components/auth/AuthGate'
 import { LlmLearningCenter } from './components/learning/LlmLearningCenter'
+import { LayoutThemeDynamicSourceEnhancer } from './components/layout/LayoutThemeDynamicSourceEnhancer'
 import './styles.css'
 import './components/learning/llm-learning.css'
+import './components/learning/nav-order-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthGate><App /><LlmLearningCenter /></AuthGate>
+    <AuthGate><App /><LlmLearningCenter /><LayoutThemeDynamicSourceEnhancer /></AuthGate>
   </React.StrictMode>
 )
