608a6cba778d87f58b43aed54cabd948228cb5f7
ynystory-gif <ynystory@gmail.com>
2026-08-28 22:50:22 +0900

    feat: require authentication before AgentStudio renders

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AuthGate } from './components/auth/AuthGate'
 import { LlmLearningCenter } from './components/learning/LlmLearningCenter'
 import './styles.css'
 import './components/learning/llm-learning.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /><LlmLearningCenter /></React.StrictMode>
+  <React.StrictMode>
+    <AuthGate><App /><LlmLearningCenter /></AuthGate>
+  </React.StrictMode>
 )
