608c68234a0939ef32b7cb71a87a38c52418daf5
ynystory-gif <ynystory@gmail.com>
2026-08-29 03:54:53 +0900

    v5.427 mount imported Theme preview styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,13 @@ import App from './App'
 import { AuthGate } from './components/auth/AuthGate'
 import { LlmLearningCenter } from './components/learning/LlmLearningCenter'
 import { LayoutThemeDynamicSourceEnhancer } from './components/layout/LayoutThemeDynamicSourceEnhancer'
+import { ImportedThemePreviewEnhancer } from './components/layout/ImportedThemePreviewEnhancer'
 import './styles.css'
 import './components/learning/llm-learning.css'
 import './components/learning/nav-order-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthGate><App /><LlmLearningCenter /><LayoutThemeDynamicSourceEnhancer /></AuthGate>
+    <AuthGate><App /><LlmLearningCenter /><LayoutThemeDynamicSourceEnhancer /><ImportedThemePreviewEnhancer /></AuthGate>
   </React.StrictMode>
 )
