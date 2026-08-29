cf9b2931ec4e877dc1a2100e489535208013cf41
ynystory-gif <ynystory@gmail.com>
2026-08-29 22:32:10 +0900

    Mount independent weight fine-tune UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { LlmLearningCenter } from './components/learning/LlmLearningCenter'
 import { LearningProblemProgressEnhancer } from './components/learning/LearningProblemProgressEnhancer'
 import { LearningPageStateRestoreEnhancer } from './components/learning/LearningPageStateRestoreEnhancer'
 import { LearningFullApplyEnhancer } from './components/learning/LearningFullApplyEnhancer'
+import { LearningWeightFinetuneEnhancer } from './components/learning/LearningWeightFinetuneEnhancer'
 import { LearningModelStackEnhancer } from './components/learning/LearningModelStackEnhancer'
 import { LearningUnlearnedListEnhancer } from './components/learning/LearningUnlearnedListEnhancer'
 import { LearningCollectionLimitEnhancer } from './components/learning/LearningCollectionLimitEnhancer'
@@ -17,10 +18,11 @@ import './styles.css'
 import './components/learning/llm-learning.css'
 import './components/learning/learning-case-list-cleanup.css'
 import './components/learning/nav-order-fix.css'
+import './components/learning/learning-weight-finetune.css'
 import './components/layout/large-layout-preview.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthGate><App /><LlmLearningCenter /><LearningProblemProgressEnhancer /><LearningPageStateRestoreEnhancer /><LearningFullApplyEnhancer /><LearningModelStackEnhancer /><LearningUnlearnedListEnhancer /><LearningCollectionLimitEnhancer /><LearningDatasetTraceEnhancer /><LayoutThemeDynamicSourceEnhancer /><ImportedThemePreviewEnhancer /><LargeLayoutPreviewEnhancer /></AuthGate>
+    <AuthGate><App /><LlmLearningCenter /><LearningProblemProgressEnhancer /><LearningPageStateRestoreEnhancer /><LearningFullApplyEnhancer /><LearningWeightFinetuneEnhancer /><LearningModelStackEnhancer /><LearningUnlearnedListEnhancer /><LearningCollectionLimitEnhancer /><LearningDatasetTraceEnhancer /><LayoutThemeDynamicSourceEnhancer /><ImportedThemePreviewEnhancer /><LargeLayoutPreviewEnhancer /></AuthGate>
   </React.StrictMode>
 )
