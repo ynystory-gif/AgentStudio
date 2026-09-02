484295d35332240d5b8e0d3af4ffa24ba1482a06
ynystory-gif <ynystorylgcns@gmail.com>
2026-09-02 10:25:06 +0900

    로컬 파일 기준 강제 동기화

--- GIT 내용을 전부 무시하고 올리기.ps1.ps1
+++ GIT 내용을 전부 무시하고 올리기.ps1.ps1
@@ -1,4 +1,16 @@
-﻿cd C:\AI\AgentStudio
+﻿# 한글 깨짐 방지==============================================
+chcp 65001
+
+[Console]::InputEncoding = [System.Text.Encoding]::UTF8
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Text.Encoding]::UTF8
+
+git config --global core.quotepath false
+git config --global i18n.commitEncoding utf-8
+git config --global i18n.logOutputEncoding utf-8
+#==============================================
+
+cd C:\AI\AgentStudio
 
 git add -A
 
